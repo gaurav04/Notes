d8177aefbb49e44445e9c19f99595b4c1629486b
gaurav04 <harsola.gaurav88@gmail.com>
2021-05-29 13:13:24 +0530

    Create fibonacci.sh

---
+++ fibonacci.sh
@@ -0,0 +1,19 @@
+
+# !/bin/bash
+
+echo -n "Enter the number: "
+  
+read N
+
+a=0
+b=1 
+   
+echo "The Fibonacci series is : "
+   
+for (( i=0; i<N; i++ ))
+do
+    echo -n "$a "
+    fn=$((a + b))
+    a=$b
+    b=$fn
+done
